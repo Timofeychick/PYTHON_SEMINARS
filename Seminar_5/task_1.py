e3f20ab50856595be47630ae87a69ffe7b36d54b
Timofeychick <androidwalletmot@gmail.com>
2023-02-26 16:08:11 +0700

    corrected tsks

--- Seminar_5/task_1.py
+++ Seminar_5/task_1.py
@@ -2,4 +2,14 @@
 # и возводит число А в целую степень B с помощью рекурсии.
 
 
-number 
+number = int(input("Введите ваше число: ")) 
+grade = int (input("В какую стпень возвести ваше число: "))
+
+def exponentiation(num , grd):
+    
+    if grd == 1:
+        return num
+    return num * exponentiation(num, grd - 1)
+
+
+print(exponentiation(number, grade))
